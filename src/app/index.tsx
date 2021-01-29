774dc848fc59be2423663b76a271a86c34eaa0cc
androsv96 <androsg93@hotmail.com>
2021-01-29 13:59:12 -0500

    added react-router-dom. Added routes for signup and login. Empty login component created.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import '../scss/application.scss';
 
 import SignUp from './screens/SignUp';
+import Login from './screens/Login';
 
 function App() {
-  return <SignUp />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/sign_up">
+          <SignUp />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
