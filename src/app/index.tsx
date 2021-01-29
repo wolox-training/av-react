ac12442bf37e44922506ffe9a524bd483643d49a
Andros Vargas <46198501+Androsv96@users.noreply.github.com>
2021-01-29 15:14:29 -0500

    Routing components (#4)
    * added Signup and CustomInput unit testing files.
    
    * added react-router-dom. Added routes for signup and login. Empty login component created.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import '../scss/application.scss';
+import { PATHS } from '../constants/paths';
 
 import SignUp from './screens/SignUp';
+import Login from './screens/Login';
 
 function App() {
-  return <SignUp />;
+  return (
+    <Router>
+      <Switch>
+        <Route path={PATHS.signup}>
+          <SignUp />
+        </Route>
+        <Route path={PATHS.login}>
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/app/screens/Login/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Login() {
+  return <span>Login, ¡coming soon!</span>;
+}
