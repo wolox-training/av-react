dec7ed8387e0ad50388e732b9914fd7e2039f810
androsv96 <androsg93@hotmail.com>
2021-02-04 19:44:14 -0500

    added pirvate and public routing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import '../scss/application.scss';
+import LocalStorageService from '~services/LocalStorageService';
+import { TOKEN_KEY } from '~utils/constants';
+
 import { PATHS } from '../constants/paths';
 
 import SignUp from './screens/SignUp';
@@ -12,14 +15,12 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path={PATHS.home}>
-          <Home />
-        </Route>
+        <Route
+          path={PATHS.home}
+          render={() => (LocalStorageService.getValue(TOKEN_KEY) ? <Home /> : <Login />)}
+        />
         <Route path={PATHS.signup}>
           <SignUp />
-        </Route>
-        <Route path={PATHS.login}>
-          <Login />
         </Route>
       </Switch>
     </Router>
--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,5 +1,5 @@
 export const PATHS = {
   login: '/',
   signup: '/sign_up',
-  home: '/home'
+  home: '/'
 };
