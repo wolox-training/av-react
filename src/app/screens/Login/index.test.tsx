ff26759c6ad24f8ff0c2d543b786b202be201024
androsv96 <androsg93@hotmail.com>
2021-02-05 16:55:31 -0500

    fixed PR testing suggestions

--- src/app/screens/Login/index.test.tsx
+++ src/app/screens/Login/index.test.tsx
@@ -9,40 +9,31 @@ const MAX_BUTTONS_LENGTH = 2;
 const MAX_ALERTS_LENGTH = 2;
 
 describe('Login test', () => {
-  test('Should match snapshot', () => {
-    const { container } = render(
+  let container: Element | null = null;
+
+  beforeEach(() => {
+    const { container: renderContainer } = render(
       <Router>
         <Login />
       </Router>
     );
+    container = renderContainer;
+  });
+
+  test('Should match snapshot', () => {
     expect(container).toMatchSnapshot();
   });
 
   test('Should render email and password inputs', () => {
-    render(
-      <Router>
-        <Login />
-      </Router>
-    );
     expect(screen.getByLabelText('Login:email')).toBeInTheDocument();
     expect(screen.getByLabelText('Login:password')).toBeInTheDocument();
   });
 
   test('Should render login and signup buttons', () => {
-    render(
-      <Router>
-        <Login />
-      </Router>
-    );
     expect(screen.getAllByRole('button').length).toBe(MAX_BUTTONS_LENGTH);
   });
 
   test('Should render required message after submitting form with empty fields', async () => {
-    render(
-      <Router>
-        <Login />
-      </Router>
-    );
     userEvent.click(screen.getByText('Login:login'));
     await waitFor(() => expect(screen.getAllByRole('alert').length).toBe(MAX_ALERTS_LENGTH));
   });
